b8b6fd38f83cc14b3b813f7e8b124cfb013322f5
Bekir Karaköse <bekirkarakose@JustMacBookPro.local>
2026-03-08 14:20:27 +0300

    YetAnotherCommitOnTerminal

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
+// window.YetAnotherGame tetiklendiğinde siteyi aç
 Object.defineProperty(window, 'YetAnotherGame', {
   get: function() {
-    window.location.href = 'justbekirsgame.justbekir.workers.dev';
+    window.location.href = 'https://justbekirsgame.justbekir.workers.dev';
     return "Opening YetAnotherGame...";
   }
 });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
